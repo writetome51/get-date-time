28087026137af0d79158d08ea21117fdd38e8999
Steve Thompson <writetome51@gmail.com>
2018-10-02 13:43:35 -0600

    added to README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Function that orders an array of strings alphabetically and returns void:
 
 alphabetize(strings);
+
+An example of result:
+
+[
+'__100',  '_10',  '-',  ',',  '!',  '?',  '.',  '(',  ')',  '@',  '*',   '/', '\\', '&',  '#',  '%',  '^',
+'+','<',  '=',  '>',  '$',  '0',  '000',  '100',  '1000',  '2',  '30',  'A',  'Â',  'Ã',  'aaa',  'aab',  'aac',
+'aardvark',  'AE',  'Ǣ',  'Atco',  'atco',  'bat',  'book',  'copy',  'i',  'Í',  'Ô',  'q',  'x',  'Zaa',
+'zaa',  'Zebra'
+]
+
--- test.ts
+++ test.ts
@@ -1,10 +1,12 @@
 import { alphabetize } from './alphabetize';
 
 
-let strings = ['book', 'copy', 'aac', '', 'aardvark', '_10', '100', 'aaa', 'atco',
-	'Atco', 'Zebra', 'zaa', 'Zaa', 'bat', '__100', '!', 'aab', '@','Ã', '#', '$', '%', '^',
-	'&', '*', '000', '(', ')', '-', '+', '=', '2', '1000', '30', '0','Ǣ', 'A', 'AE'];
+let strings = ['book', 'copy', 'zaa', 'aac', '_10', '100', 'aaa', 'atco',
+	'Zebra', 'bat', '__100', '!', 'aab', '@', 'Ã', '#', '$', '%', '^',
+	'&', '*', '000', 'AE', '(', ')', '-', 'Atco', '+', '=', 'aardvark', '2',
+	'1000', '30', '0', 'A', 'qqq','~', 'x', 'Ô', 'Â', '<', '>', ',', '.', 'Í', 'Ǣ',
+	'i', '/', '?', 'Zaa','∏', '|','\\'];
 
-alphabetize('');
+alphabetize(strings);
 
 console.log(strings);
